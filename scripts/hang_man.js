ed8b75e7f5be7e726b356d16a028a0e0cff1bda6
Cheryl Hankins <cheryl.hankins@att.net>
2017-04-17 09:42:54 -0500

    add some beginning styling

--- scripts/hang_man.js
+++ scripts/hang_man.js
@@ -10,10 +10,12 @@ var launchCodeWords = ["launchCode", "javaScript",
       "version control", "hypertext markup language",
       "attribute", "developer", "command line"]
 
-// select random entry from launchCodeWords
+// select random entry from launchCodeWords array
 function getLaunchCodeWords() {
    rand = [Math.floor(Math.random() * launchCodeWords.length)];
    word = launchCodeWords[rand];
+   $("#game_word").html(word);
+   $("#game_word").hide();
    hangman();
    }
 
@@ -26,12 +28,18 @@ function hangman(){
             answerArray [i] = "\u00A0 \u00A0";
         }
         else{
-            answerArray [i] = "_ ";
+            answerArray [i] = "___ ";
         }
-      $("#game").html(answerArray);
+      $("#game_blanks").html(answerArray);
   }
 }
-
+//start new game
+function newGame() {
+    $("#playAgainButton").click(function(event){
+        getLaunchCodeWords();
+        console.log("New game");
+    });
+}
 console.log(getLaunchCodeWords());
 console.log(word);
 console.log("end");
