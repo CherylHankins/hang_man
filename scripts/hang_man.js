83eacc6051af7bd3cebd03fbca3a0a82ab348f68
Cheryl Hankins <cheryl.hankins@att.net>
2017-04-17 23:17:09 -0500

    complete basic functions of game

--- scripts/hang_man.js
+++ scripts/hang_man.js
@@ -1,6 +1,18 @@
 console.log("start");
-// things we need to keep track of
 
+// things we need to keep track of
+var model= {
+  // individual letters of current Game Word
+  gameWord: [],
+  // individual blank lines of current Game Word
+  gameLines: [],
+  // Total lives for game
+  lives: 7,
+  //numInWordBank: Words.length,
+  // current Game Word
+  word: "",
+  wordLine: ""
+}
 // list of game words
 var launchCodeWords = ["launchCode", "javaScript",
       "python", "cascading style sheet", "document",
@@ -11,35 +23,221 @@ var launchCodeWords = ["launchCode", "javaScript",
       "attribute", "developer", "command line"]
 
 // select random entry from launchCodeWords array
-function getLaunchCodeWords() {
-   rand = [Math.floor(Math.random() * launchCodeWords.length)];
-   word = launchCodeWords[rand];
-   $("#game_word").html(word);
-   $("#game_word").hide();
-   hangman();
-   }
+function getLaunchCodeWord() {
+  rand = [Math.floor(Math.random() * launchCodeWords.length)];
+  model.word = launchCodeWords[rand];
+  //gameLetters = word.split("");
+  //model.gameWord.push(gameLetters);
+  //setGameboard();
+  }
 
 // create game board with appropriate blank lines
 // and spaces between words
-function hangman(){
-    var answerArray = [];
-    for (var i= 0; i< word.length; i++) {
-        if (word[i] === " "){
-            answerArray [i] = "\u00A0 \u00A0";
-        }
-        else{
-            answerArray [i] = "___ ";
-        }
-      $("#game_blanks").html(answerArray);
+function setGameboard(){
+  getLaunchCodeWord();
+  //var answerArray = [];
+  for (var i= 0; i< model.word.length; i++) {
+      //if (word[i] === " "){
+          //answerArray [i] = "\u00A0 \u00A0";
+      //}
+      //else{
+        //  answerArray [i] = "___ ";
+      //}
+    //$("#game_blanks").html(answerArray);
+    model.gameWord[i] = model.word.charAt(i);
+    model.gameLines[i] = "_ ";
+  }
+  //model.wordLine = model.gameLines.join("");
+  $("#WORD").html(model.gameLines);
+  $("#numLetters").html(model.word.length);
+}
+
+function updateLetter(letter) {
+  var changes = 0;
+  for (var i= 0; i< model.word.length; i++) {
+    model.gameWord[i] = model.word.charAt(i);
+    if (model.word.charAt(i) == letter) {
+      model.gameLines[i] = letter;
+      changes = changes + 1;
+    }
+  }
+  if(changes < 1) {
+    model.lives -= 1;
+    $("#lives").html(model.lives);
+  }
+  model.wordLine = model.gameLines.join("");
+  $("#WORD").html(model.wordLine);
+
+  var word1 = model.gameWord.join("");
+  var word2 = model.gameLines.join("");
+
+  if(word1 == word2) {
+    alert("Winner");
+  }
+  if (model.lives < 1) {
+    $("#WORD").html(word1);
+    alert("Loser");
   }
 }
-//start new game
-function newGame() {
-    $("#playAgainButton").click(function(event){
-        getLaunchCodeWords();
-        console.log("New game");
-    });
-}
-console.log(getLaunchCodeWords());
-console.log(word);
+
+$(function() {
+  $("#a").click(function(event){
+    event.preventDefault();
+    updateLetter("a");
+  });
+});
+$(function() {
+  $("#b").click(function(event){
+    event.preventDefault();
+    updateLetter("b");
+  });
+});
+$(function() {
+  $("#c").click(function(event){
+    event.preventDefault();
+    updateLetter("c");
+  });
+});
+$(function() {
+  $("#d").click(function(event){
+    event.preventDefault();
+    updateLetter("d");
+  });
+});
+$(function() {
+  $("#e").click(function(event){
+    event.preventDefault();
+    updateLetter("e");
+  });
+});
+$(function() {
+  $("#f").click(function(event){
+    event.preventDefault();
+    updateLetter("f");
+  });
+});
+$(function() {
+  $("#g").click(function(event){
+    event.preventDefault();
+    updateLetter("g");
+  });
+});
+$(function() {
+  $("#h").click(function(event){
+    event.preventDefault();
+    updateLetter("h");
+  });
+});
+$(function() {
+  $("#i").click(function(event){
+    event.preventDefault();
+    updateLetter("i");
+  });
+});
+$(function() {
+  $("#j").click(function(event){
+    event.preventDefault();
+    updateLetter("j");
+  });
+});
+$(function() {
+  $("#k").click(function(event){
+    event.preventDefault();
+    updateLetter("k");
+  });
+});
+$(function() {
+  $("#l").click(function(event){
+    event.preventDefault();
+    updateLetter("l");
+  });
+});
+$(function() {
+  $("#m").click(function(event){
+    event.preventDefault();
+    updateLetter("m");
+  });
+});
+$(function() {
+  $("#n").click(function(event){
+    event.preventDefault();
+    updateLetter("n");
+  });
+});
+$(function() {
+  $("#o").click(function(event){
+    event.preventDefault();
+    updateLetter("o");
+  });
+});
+$(function() {
+  $("#p").click(function(event){
+    event.preventDefault();
+    updateLetter("p");
+  });
+});
+$(function() {
+  $("#q").click(function(event){
+    event.preventDefault();
+    updateLetter("q");
+  });
+});
+$(function() {
+  $("#r").click(function(event){
+    event.preventDefault();
+    updateLetter("r");
+  });
+});
+$(function() {
+  $("#s").click(function(event){
+    event.preventDefault();
+    updateLetter("s");
+  });
+});
+$(function() {
+  $("#t").click(function(event){
+    event.preventDefault();
+    updateLetter("t");
+  });
+});
+$(function() {
+  $("#u").click(function(event){
+    event.preventDefault();
+    updateLetter("u");
+  });
+});
+$(function() {
+  $("#v").click(function(event){
+    event.preventDefault();
+    updateLetter("v");
+  });
+});
+$(function() {
+  $("#w").click(function(event){
+    event.preventDefault();
+    updateLetter("w");
+  });
+});
+$(function() {
+  $("#x").click(function(event){
+    event.preventDefault();
+    updateLetter("x");
+  });
+});
+$(function() {
+  $("#y").click(function(event){
+    event.preventDefault();
+    updateLetter("y");
+  });
+});
+$(function() {
+  $("#z").click(function(event){
+    event.preventDefault();
+    updateLetter("z");
+  });
+});
+
+console.log(getLaunchCodeWord());
+console.log(setGameboard());
+console.log(updateLetter());
 console.log("end");
