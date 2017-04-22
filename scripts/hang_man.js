9c6b843ad68022cf616a460866df545db8b2f602
Cheryl Hankins <cheryl.hankins@att.net>
2017-04-22 16:51:58 -0500

    add canvas hangman images hookup new game button

--- scripts/hang_man.js
+++ scripts/hang_man.js
@@ -8,13 +8,15 @@ var model= {
   gameLines: [],
   // Total lives for game
   lives: 7,
-  //numInWordBank: Words.length,
+  //number of incorrect guesses
+  numWrong: 0,
   // current Game Word
   word: "",
   wordLine: ""
 }
+
 // list of game words
-var launchCodeWords = ["launchCode", "javaScript",
+var launchCodeWords = ["launchcode", "javascript",
       "python", "cascading style sheet", "document",
       "object oriented programming", "function",
       "server", "frontend", "backend", "attribute",
@@ -26,32 +28,24 @@ var launchCodeWords = ["launchCode", "javaScript",
 function getLaunchCodeWord() {
   rand = [Math.floor(Math.random() * launchCodeWords.length)];
   model.word = launchCodeWords[rand];
-  //gameLetters = word.split("");
-  //model.gameWord.push(gameLetters);
-  //setGameboard();
-  }
+}
 
 // create game board with appropriate blank lines
 // and spaces between words
 function setGameboard(){
+  hangman();
   getLaunchCodeWord();
-  //var answerArray = [];
   for (var i= 0; i< model.word.length; i++) {
-      //if (word[i] === " "){
-          //answerArray [i] = "\u00A0 \u00A0";
-      //}
-      //else{
-        //  answerArray [i] = "___ ";
-      //}
-    //$("#game_blanks").html(answerArray);
     model.gameWord[i] = model.word.charAt(i);
+    if (model.gameWord[i] === " "){
+      model.gameLines[i] = "\u00A0\u00A0";
+    }else{
     model.gameLines[i] = "_ ";
+   }
   }
-  //model.wordLine = model.gameLines.join("");
-  $("#WORD").html(model.gameLines);
-  $("#numLetters").html(model.word.length);
 }
 
+// update letter in Gameword or hangman image
 function updateLetter(letter) {
   var changes = 0;
   for (var i= 0; i< model.word.length; i++) {
@@ -61,181 +55,358 @@ function updateLetter(letter) {
       changes = changes + 1;
     }
   }
+
   if(changes < 1) {
     model.lives -= 1;
+    model.numWrong = model.numWrong + 1;
     $("#lives").html(model.lives);
   }
+
   model.wordLine = model.gameLines.join("");
   $("#WORD").html(model.wordLine);
 
   var word1 = model.gameWord.join("");
   var word2 = model.gameLines.join("");
 
+  word1 = word1.replace(/\s/g, '');
+  word2 = word2.replace(/\s/g, '');
+
+  if(model.numWrong == 2){
+    head();
+  }
+  if(model.numWrong == 3){
+    body();
+  }
+  if(model.numWrong == 4){
+    leftArm();
+  }
+  if(model.numWrong == 5){
+    rightArm();
+  }
+  if(model.numWrong == 6){
+    leftLeg();
+  }
+  if(model.numWrong == 7){
+    rightLeg();
+  }
   if(word1 == word2) {
     alert("Winner");
   }
   if (model.lives < 1) {
-    $("#WORD").html(word1);
+    $("#WORD").html(model.word);
     alert("Loser");
   }
 }
 
+//clear hangman image
+function clearCanvas(){
+  var c = document.getElementById("myCanvas");
+  var ctx = c.getContext("2d");
+  ctx.beginPath();
+  ctx.save();
+  ctx.setTransform(1,0,0,1,0,0);
+  ctx.clearRect(0, 0, 200, 100);
+  ctx.restore();
+}
+
+//start new game
+function newGame(){
+  model.gameWord = [];
+  model.gameLines = [];
+  model.lives = 6;
+  model.word = "";
+  model.wordLine = "";
+  model.numWrong = 1;
+  $("#lives").html(model.lives);
+  clearCanvas();
+  resetLetterBank();
+  setGameboard();
+  $("#WORD").html(model.gameLines);
+}
+
+//reset letterbank for new game
+function resetLetterBank(){
+  $("#a").removeClass("disabled");
+  $("#b").removeClass("disabled");
+  $("#c").removeClass("disabled");
+  $("#d").removeClass("disabled");
+  $("#e").removeClass("disabled");
+  $("#f").removeClass("disabled");
+  $("#g").removeClass("disabled");
+  $("#h").removeClass("disabled");
+  $("#i").removeClass("disabled");
+  $("#j").removeClass("disabled");
+  $("#k").removeClass("disabled");
+  $("#l").removeClass("disabled");
+  $("#m").removeClass("disabled");
+  $("#n").removeClass("disabled");
+  $("#o").removeClass("disabled");
+  $("#p").removeClass("disabled");
+  $("#q").removeClass("disabled");
+  $("#r").removeClass("disabled");
+  $("#s").removeClass("disabled");
+  $("#t").removeClass("disabled");
+  $("#u").removeClass("disabled");
+  $("#v").removeClass("disabled");
+  $("#w").removeClass("disabled");
+  $("#x").removeClass("disabled");
+  $("#y").removeClass("disabled");
+  $("#z").removeClass("disabled");
+}
+
+//read letter selection and disable button
 $(function() {
   $("#a").click(function(event){
-    event.preventDefault();
     updateLetter("a");
+    $("#a").addClass("disabled");
   });
 });
 $(function() {
   $("#b").click(function(event){
-    event.preventDefault();
     updateLetter("b");
+    $("#b").addClass("disabled");
   });
 });
 $(function() {
   $("#c").click(function(event){
-    event.preventDefault();
     updateLetter("c");
+    $("#c").addClass("disabled");
   });
 });
 $(function() {
   $("#d").click(function(event){
-    event.preventDefault();
-    updateLetter("d");
+      updateLetter("d");
+    $("#d").addClass("disabled");
   });
 });
 $(function() {
   $("#e").click(function(event){
-    event.preventDefault();
-    updateLetter("e");
+      updateLetter("e");
+    $("#e").addClass("disabled");
   });
 });
 $(function() {
   $("#f").click(function(event){
-    event.preventDefault();
     updateLetter("f");
+    $("#f").addClass("disabled");
   });
 });
 $(function() {
   $("#g").click(function(event){
-    event.preventDefault();
-    updateLetter("g");
+      updateLetter("g");
+    $("#g").addClass("disabled");
   });
 });
 $(function() {
   $("#h").click(function(event){
-    event.preventDefault();
-    updateLetter("h");
+        updateLetter("h");
+    $("#h").addClass("disabled");
   });
 });
 $(function() {
   $("#i").click(function(event){
-    event.preventDefault();
     updateLetter("i");
+    $("#i").addClass("disabled");
   });
 });
 $(function() {
   $("#j").click(function(event){
-    event.preventDefault();
-    updateLetter("j");
+      updateLetter("j");
+    $("#j").addClass("disabled");
   });
 });
 $(function() {
   $("#k").click(function(event){
-    event.preventDefault();
     updateLetter("k");
+    $("#k").addClass("disabled");
   });
 });
 $(function() {
   $("#l").click(function(event){
     event.preventDefault();
     updateLetter("l");
+    $("#l").addClass("disabled");
   });
 });
 $(function() {
   $("#m").click(function(event){
     event.preventDefault();
     updateLetter("m");
+    $("#m").addClass("disabled");
   });
 });
 $(function() {
   $("#n").click(function(event){
     event.preventDefault();
     updateLetter("n");
+    $("#n").addClass("disabled");
   });
 });
 $(function() {
   $("#o").click(function(event){
     event.preventDefault();
     updateLetter("o");
+    $("#o").addClass("disabled");
   });
 });
 $(function() {
   $("#p").click(function(event){
     event.preventDefault();
     updateLetter("p");
+    $("#p").addClass("disabled");
   });
 });
 $(function() {
   $("#q").click(function(event){
     event.preventDefault();
     updateLetter("q");
+    $("#q").addClass("disabled");
   });
 });
 $(function() {
   $("#r").click(function(event){
     event.preventDefault();
     updateLetter("r");
+    $("#r").addClass("disabled");
   });
 });
 $(function() {
   $("#s").click(function(event){
     event.preventDefault();
     updateLetter("s");
+    $("#s").addClass("disabled");
   });
 });
 $(function() {
   $("#t").click(function(event){
     event.preventDefault();
     updateLetter("t");
+    $("#t").addClass("disabled");
   });
 });
 $(function() {
   $("#u").click(function(event){
     event.preventDefault();
     updateLetter("u");
+    $("#u").addClass("disabled");
   });
 });
 $(function() {
   $("#v").click(function(event){
     event.preventDefault();
     updateLetter("v");
+    $("#v").addClass("disabled");
   });
 });
 $(function() {
   $("#w").click(function(event){
     event.preventDefault();
     updateLetter("w");
+    $("#w").addClass("disabled");
   });
 });
 $(function() {
   $("#x").click(function(event){
     event.preventDefault();
     updateLetter("x");
+    $("#x").addClass("disabled");
   });
 });
 $(function() {
   $("#y").click(function(event){
     event.preventDefault();
     updateLetter("y");
+    $("#y").addClass("disabled");
   });
 });
 $(function() {
   $("#z").click(function(event){
     event.preventDefault();
     updateLetter("z");
+    $("#z").addClass("disabled");
   });
 });
+
+//DRAW HANGMAN body parts
+//starting hangman image
+//bottom line
+function hangman(){
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+    ctx.moveTo(70,90);
+    ctx.lineTo(120,90);
+    ctx.stroke();
+    //left side
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+    ctx.moveTo(70,90);
+    ctx.lineTo(70,10);
+    ctx.stroke();
+    //top bar
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+    ctx.moveTo(70,10);
+    ctx.lineTo(100,10);
+    ctx.stroke();
+    //noose
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+    ctx.moveTo(100,10);
+    ctx.lineTo(100,20);
+    ctx.stroke();
+}
+function head(){
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+    ctx.beginPath();
+    ctx.arc(100,30,10,0,2*Math.PI);
+    ctx.stroke();
+    ctx.moveTo(95,27);
+    ctx.lineTo(95,28);
+    ctx.stroke();
+    ctx.moveTo(105,27);
+    ctx.lineTo(105,28);
+    ctx.stroke();
+    ctx.moveTo(97,34);
+    ctx.lineTo(103,34);
+    ctx.stroke();
+    ctx.closePath();
+}
+function body(){
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+    ctx.moveTo(100,40);
+    ctx.lineTo(100,70);
+    ctx.stroke();
+}
+function leftArm(){
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+    ctx.moveTo(100,50);
+    ctx.lineTo(85,60);
+    ctx.stroke();
+}
+function rightArm(){
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+    ctx.moveTo(100,50);
+    ctx.lineTo(115,60);
+    ctx.stroke();
+}
+function leftLeg(){
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+    ctx.moveTo(100,70);
+    ctx.lineTo(85,80);
+    ctx.stroke();
+}
+function rightLeg(){
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+    ctx.moveTo(100,70);
+    ctx.lineTo(115,80);
+    ctx.stroke();
+}
 
 console.log(getLaunchCodeWord());
 console.log(setGameboard());
